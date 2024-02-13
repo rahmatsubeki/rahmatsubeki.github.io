450606c413c81e3d2cd64528fc5f15b5f81bf150
beky <91580555+rahmatsubeki@users.noreply.github.com>
2024-02-13 18:43:56 +0700

    Update script.js
    view web

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,20 @@
 document.getElementById('tahun').innerText = new Date().getFullYear();
+
+document.getElementById('openProjectBtn').addEventListener('click', function () {
+    const websiteUrl = this.getAttribute('data-website');
+
+    if (websiteUrl) {
+        Swal.fire({
+            html: `<iframe src="${websiteUrl}" width="100%" height="500px"></iframe>`,
+            showCloseButton: true,
+            showConfirmButton: false,
+        });
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'URL website tidak ditemukan!',
+        });
+    }
+});
+
