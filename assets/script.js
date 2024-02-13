c281c213d8b833eda4225a6f3f787d2e69dd78da
beky <91580555+rahmatsubeki@users.noreply.github.com>
2024-02-14 00:03:02 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,24 @@
 document.getElementById('tahun').innerText = new Date().getFullYear();
+document.querySelectorAll('#openProjectBtn').forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        const websiteUrl = this.getAttribute('data-website');
 
-document.getElementById('openProjectBtn').addEventListener('click', function () {
-    const websiteUrl = this.getAttribute('data-website');
-
-    if (websiteUrl) {
-        Swal.fire({
-            html: `<iframe src="${websiteUrl}" width="100%" height="500px"></iframe>`,
-            showCloseButton: true,
-            showConfirmButton: false,
-        });
-    } else {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops...',
-            text: 'URL website tidak ditemukan!',
-        });
-    }
+        if (websiteUrl) {
+            Swal.fire({
+                html: `<iframe src="${websiteUrl}" width="100%" height="500px"></iframe>`,
+                showCloseButton: true,
+                showConfirmButton: false,
+            });
+        } else {
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'URL website tidak ditemukan!',
+            });
+        }
+    });
 });
+
+
+
 
